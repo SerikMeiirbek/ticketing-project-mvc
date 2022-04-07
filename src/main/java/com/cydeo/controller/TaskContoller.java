cf36f7b3111a58fac2990da3b1f377174c956251
serik.meiirbek@gmail.com <serik.meiirbek@gmail.com>
2022-04-07 17:46:36 -0400

    Part-4

--- src/main/java/com/cydeo/controller/TaskContoller.java
+++ src/main/java/com/cydeo/controller/TaskContoller.java
@@ -1,21 +1,45 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
+import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/task")
 public class TaskContoller {
 
+    private final TaskService taskService;
+    private final ProjectService projectService;
+    private final UserService userService;
+
+    public TaskContoller(TaskService taskService, ProjectService projectService, UserService userService) {
+        this.taskService = taskService;
+        this.projectService = projectService;
+        this.userService = userService;
+    }
+
     @GetMapping("/create")
     public String taskCreate(Model model){
 
-//        model.addAttribute("projects", projectService.findAll());
-//        model.addAttribute("managers", userService.findManagers());
+        model.addAttribute("task", new TaskDTO());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("tasks", taskService.findAll());
 
         return "/task/create";
     }
+
+    @PostMapping("/create")
+    public String taskInsert(@ModelAttribute("task") TaskDTO task){
+        taskService.save(task);
+        return "redirect:/task/update";
+    }
+
+
 }
