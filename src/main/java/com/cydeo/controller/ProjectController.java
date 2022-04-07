8d2847d9226bb9777f29bce1b172fa4cc714abae
serik.meiirbek@gmail.com <serik.meiirbek@gmail.com>
2022-04-07 07:21:12 -0400

    Part-3

---
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -0,0 +1,46 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+    ProjectService projectService;
+    UserService userService;
+
+    public ProjectController(ProjectService projectService, UserService userService) {
+        this.projectService = projectService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/create")
+    public String projectCreate(Model model){
+
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("projects", projectService.findAll());
+        model.addAttribute("managers", userService.findAll());
+
+        return "/project/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(@ModelAttribute("project") ProjectDTO project, Model model){
+
+        projectService.save(project);
+
+        return "redirect:/project/create";
+
+    }
+
+}
